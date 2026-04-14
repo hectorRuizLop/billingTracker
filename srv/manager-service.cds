eb6bd01cee10011ff5122c97d20798a955772a98
Hector Ruiz <hector.ruiz@nubexx.es>
2026-04-14 09:01:25 +0000

    Service for manager and csv mock data and http and requests for testing

---
+++ srv/manager-service.cds
@@ -0,0 +1,26 @@
+using {my.billing as db} from '../db/schema';
+
+service ManagerService @(path: '/api/manager')@(requires: 'Manager') {
+
+    @(restrict: [{
+        grant: '*',
+        where: 'manager_ID = $user.id'
+    }])
+    entity Projects           as projection on db.Projects;
+
+    @(restrict: [{
+        grant: 'READ',
+        where: 'project.manager_ID = $user.id'
+    }])
+    entity TimeEntries        as
+        projection on db.TimeEntries {
+            *,
+            (employee.firstName || ' ' || employee.lastName) as employeeName : String,
+            employee.category.name as categoryName,
+            project.name           as projectName,
+            hours * rateSnapshot   as cost : Decimal(15, 2)
+        };
+
+    entity ProjectAssignments as projection on db.ProjectAssignments;
+
+}
---
+++ srv/manager-service.js
@@ -0,0 +1,30 @@
+const cds = require('@sap/cds');
+
+module.exports = class ManagerService extends cds.ApplicationService {
+
+  async init() {
+    const db = await cds.connect.to('db');
+    const { Employees, Categories } = db.entities('my.billing');
+
+    // Auto-fill rateSnapshot, month, year on TimeEntry creation
+    this.before('CREATE', 'TimeEntries', async (req) => {
+      const { employee_ID, date } = req.data;
+
+      if (date) {
+        const d = new Date(date);
+        req.data.month = d.getUTCMonth() + 1;
+        req.data.year  = d.getUTCFullYear();
+      }
+
+      if (employee_ID && !req.data.rateSnapshot) {
+        const employee = await SELECT.one.from(Employees).where({ ID: employee_ID });
+        if (employee?.category_ID) {
+          const category = await SELECT.one.from(Categories).where({ ID: employee.category_ID });
+          if (category?.rate) req.data.rateSnapshot = category.rate;
+        }
+      }
+    });
+    await super.init();
+  }
+
+};
